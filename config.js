3fa82d1d24e1a937f53b806924392eb415611ea5
Nam2528 <139524140+Nam2528@users.noreply.github.com>
2024-02-09 18:52:26 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=61555401596477';
+const eventName = 'Chúc Mừng Năm mới';
+const titleCard = 'Tặng Em yêu';
+const contentCard = 'Trong bước chuyển giao giữa năm cũ và năm mới, anh muốn dành những dòng chữ này để gửi đến em, người đã là tia sáng của cuộc đời anh, Những ngày bên em là những khoảnh khắc đáng trân trọng nhất đối với anh.Năm mới này, anh chúc em ngày càng xinh đẹp và gặp được thật nhiều điều may mắn và tốt lành. Chúc em và gia đình có một năm mới đầy ắp niềm vui, hạnh phúc và thành công của cuộc sống. cảm ơn em vì những khoảnh khắc tuyệt vời mà chúng ta đã cùng nhau trải qua và mong rằng chúng ta sẽ có nhiều hơn nữa trong năm mới này.YÊU EM !';
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
